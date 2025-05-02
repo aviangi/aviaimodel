d552b00c0ae31b2b0e6f4714d352fe5d51767235
aviangi <angiwatsoncoding@gmail.com>
2025-05-03 04:51:44 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+// this ai is built by avishek
+
+import { Assistant } from "./assistant"; // Make sure this path is correct for your project
+
+export default function Home() {
+  return <Assistant />;
+}
